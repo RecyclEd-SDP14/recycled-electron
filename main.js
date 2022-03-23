dc25c32082571a87c59471077c00327f2e1c9385
Oliver Smith <01smito01@gmail.com>
2022-03-23 13:01:30 +0000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
