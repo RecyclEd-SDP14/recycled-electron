90aed791bf390902089f94693d62677da2c4a336
s1911393 <s.t.florence@sms.ed.ac.uk>
2022-03-29 17:23:19 +0100

    whoop

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const fs = require('fs');
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
@@ -17,21 +17,7 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
-const curr_stats = {
-    "plastic": 0,
-    "glass": 0,
-    "can": 0,
-    "trash": 0
-};
 
-const data = JSON.stringify(curr_stats);
-
-fs.writeFile('curr_stats.json', data, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("JSON data is saved.");
-});
 
 app.whenReady().then(() => {
     createWindow()
