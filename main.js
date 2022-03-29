14c6268918133e210caec2582cae9c43fc455c02
s1911393 <s.t.florence@sms.ed.ac.uk>
2022-03-29 15:17:16 +0100

    added totals for each type

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const fs = require('fs');
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
@@ -8,11 +9,28 @@ const createWindow = () => {
         autoHideMenuBar: true,
         frame: false,
         webPreferences: {
+            nodeIntegration: true, 
+            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
         }
     })
     win.loadFile('index.html')
 }
+
+const curr_stats = {
+    "plastic": 0,
+    "glass": 0,
+    "can": 0
+};
+
+const data = JSON.stringify(curr_stats);
+
+fs.writeFile('curr_stats.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
 
 app.whenReady().then(() => {
     createWindow()
